297ccb096d4394f4854a3db05ab23dd8a6a8532d
yuenthon191 <62110706+yuenthon191@users.noreply.github.com>
2020-05-25 16:46:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ console.log('-----ปริ้นทั้งหมด-----' + JSON.stringify(da
 function data_enter(sende_r, beacon_id) {
 
 
-   var checktime = "https://script.google.com/macros/s/AKfycbxtDBsksskxoSwLYklM1GRbqgktqFtfvIEf2XY/exec?userId="+sende_r+"&beaconid="+beacon_id;
+   var checktime = "https://script.google.com/macros/s/AKfycbxxDwUO-Z2ch6UYBdqS0sj_N7HFFkGqJa2Ab6Hk/exec?userId="+sende_r+"&beaconid="+beacon_id;
  request(checktime, function (error, response, body) {
 
 })
@@ -108,17 +108,17 @@ var options = {
         var info = JSON.parse(body);
         name = info.displayName;
         
-          if(beacon_id == "013d8453ee"){
+          if(beacon_id == "013dfbdd50"){
          var msg = {
            "type": "text",
-           "text": "ระบบได้บันทึกการตรวจจุดตรวจสำนักงานนิติบุคคลของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
+           "text": "ระบบได้บันทึกการตรวจจุดตรวจ Alk. scrubber ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
 
           }; 
         reply(reToken, msg, sende_r, beacon_id);    
-         }else if(beacon_id == "013d876560"){
+         }else if(beacon_id == "013e02417c"){
          var msg = {
            "type": "text",
-           "text": "ระบบได้บันทึกการตรวจจุดตรวจตำแหน่ง TT78 ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
+           "text": "ระบบได้บันทึกการตรวจจุดตรวจด้านหลังโรงงานของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
           }; 
         reply(reToken, msg, sende_r, beacon_id);   
          }
